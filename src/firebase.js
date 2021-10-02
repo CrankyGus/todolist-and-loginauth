67db86f742c390f757cda167c0247485f78fb9a5
CrankyGus <38606495+CrankyGus@users.noreply.github.com>
2021-10-03 06:14:08 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,16 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
+
+//เอา project setting จาก firebase มาใส่
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyBTtGhHPD-wzG7Im_ebkBd3dfOjAbh0kYk",
-    authDomain: "auth-dev-44bab.firebaseapp.com",
-    databaseURL: "https://auth-dev-44bab-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "auth-dev-44bab",
-    storageBucket: "auth-dev-44bab.appspot.com",
-    messagingSenderId: "401622559600",
-    appId: "1:401622559600:web:546f6bd02aad25e25d3db5"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 })
 
 export const auth = app.auth()
